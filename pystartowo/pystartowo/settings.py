b1f51cc2ccd7346d44bc82b4f4aa62c133f71e0c
Rafał Kowalski <rafkow91@gmail.com>
2021-10-21 15:55:29 +0200

    change django secret key

--- pystartowo/pystartowo/settings.py
+++ pystartowo/pystartowo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^ij0r33fy!#jf#(7k2(_ov-xf)pefpp_ms7)ly$^q1il-b8(n'
+SECRET_KEY = 'django'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
