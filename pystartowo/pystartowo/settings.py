5e8d588425999233e3f9420f3d45f336a1d3d962
Rafał Kowalski <rafkow91@gmail.com>
2021-12-11 21:39:44 +0100

    small fix

--- pystartowo/pystartowo/settings.py
+++ pystartowo/pystartowo/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'app',
         'PASSWORD': 'admin',
-        'HOST': 'db',
+        'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
