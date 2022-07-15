78470188b699f85034c968529fabe9b116ad71cc
Rafał Kowalski <rafkow91@yahoo.com>
2022-07-15 23:38:25 +0200

    new start of project

--- pystartowo/knowledge/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserProfile
-
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
